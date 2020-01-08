853e87ef944cdb6d0b3c8e426468e379b3f9a775
BoJiang96 <59638134+BoJiang96@users.noreply.github.com>
2020-01-08 14:29:59 +0800

    Add files via upload

---
+++ AGREE/config.py
@@ -0,0 +1,20 @@
+'''
+Created on Nov 10, 2017
+Store parameters
+
+@author: Lianhai Miao
+'''
+
+class Config(object):
+    def __init__(self):
+        self.path = './data/ml-1m/'
+        self.user_dataset = self.path + 'userRating'
+        self.group_dataset = self.path + 'groupRating'
+        self.user_in_group_path = "./data/ml-1m/groupMember.txt"
+        self.embedding_size = 64
+        self.epoch = 30
+        self.num_negatives = 4
+        self.batch_size = 2048
+        self.lr = [0.000005, 0.000001, 0.0000005]
+        self.drop_ratio = 0
+        self.topK = 5
