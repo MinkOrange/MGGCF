fd01c0e11a2898d40f0cc80455a918d9aef668f4
BoJiang96 <59638134+BoJiang96@users.noreply.github.com>
2020-01-10 15:18:33 +0800

    Update config.py

--- AGREE/config.py
+++ AGREE/config.py
@@ -16,5 +16,5 @@ class Config(object):
         self.num_negatives = 4
         self.batch_size = 2048
         self.lr = [0.000005, 0.000001, 0.0000005]
-        self.drop_ratio = 0
+        self.drop_ratio = 0.2
         self.topK = 5
