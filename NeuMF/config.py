ecb3e3e5bc489d8702fbbf1260701b6a56bcbc6f
BoJiang96 <59638134+BoJiang96@users.noreply.github.com>
2020-01-10 16:16:50 +0800

    Update config.py

--- NeuMF/config.py
+++ NeuMF/config.py
@@ -1,9 +1,4 @@
-'''
-Created on Nov 10, 2017
-Store parameters
 
-@author: Lianhai Miao
-'''
 
 class Config(object):
     def __init__(self):
@@ -17,5 +12,5 @@ class Config(object):
         self.num_negatives = 4
         self.batch_size = 2048
         self.lr = [0.000005, 0.000001, 0.0000005]
-        self.drop_ratio = 0
+        self.drop_ratio = 0.2
         self.topK = 5
